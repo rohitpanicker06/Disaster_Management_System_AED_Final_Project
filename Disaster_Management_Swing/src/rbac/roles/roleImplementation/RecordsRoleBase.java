81c82e571dbf5d684586bab9d07d6dba95e9f682
rohitpanicker06@gmail.com <rohitpanicker06@gmail.com>
2022-11-28 19:01:13 -0500

    Added Role Interface and implementation for Site Reporting Employee, Civil Response Head, Disaster Management Head, Fund Raiser Employee, Pharmacy and Records Employee

---
+++ Disaster_Management_Swing/src/rbac/roles/roleImplementation/RecordsRoleBase.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rbac.roles.roleImplementation;
+
+import rbac.role.Role;
+import rbac.roles.roleInterface.RecordsHeadRoleInterface;
+/**
+ *
+ * @author rohitpanicker
+ */
+public class RecordsRoleBase extends Role implements RecordsHeadRoleInterface{
+    
+}
