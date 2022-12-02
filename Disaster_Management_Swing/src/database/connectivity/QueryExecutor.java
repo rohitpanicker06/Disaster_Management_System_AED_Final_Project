763ebc2b118035056b08d220c6d2efc72a29e1c5
rohitpanicker06@gmail.com <rohitpanicker06@gmail.com>
2022-12-02 13:15:09 -0500

    Removing unwanted main class for QueryExecutor class

--- Disaster_Management_Swing/src/database/connectivity/QueryExecutor.java
+++ Disaster_Management_Swing/src/database/connectivity/QueryExecutor.java
@@ -18,17 +18,6 @@ import java.sql.*;
 public class QueryExecutor {
     
     
-   
-    public static void main(String[] args) throws SQLException {
-        
-       
-        ResultSet set = executeQuery("Select * from Diseases where SLNO=2");    
-        while(set.next()) {
-        System.out.println(set.getInt(1)+"  "+set.getString(2)+"  "+set.getString(3));  
-        }   
-    }
-    
-    
     public static ResultSet executeQuery(String queryString) throws SQLException
     {
          Connection dbCon = DataBaseConnection.getInstance().getConnectionInstance();
